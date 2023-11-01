4a2ad4ab0db420eb3a7cc02cc766d6112b9bc3e9
Ivan-Giovanni <zanguegiovanni@gmail.com>
2023-11-01 19:49:46 +0100

    DataFrame
    Signed-off-by: Ivan-Giovanni <zanguegiovanni@gmail.com>

--- Hopital.py
+++ Hopital.py
@@ -4,16 +4,16 @@ from tkinter import messagebox
 import mysql.connector
 
 
-#============================================== Définition de la classe Hopital ====================================================#
+# ============================================== Définition de la classe Hopital =============================================== #
 
 class Hopital:
     def __init__(self, root):
-        #========== Définition de la fenêtre =========================#
+        # ==================================== Définition de la fenêtre ======================================================= #
         self.root = root
         self.root.title("Hopital Les Génies Management System")
         self.root.geometry("1540x800+0+0")
 
-        #=========== Déclaration des variables ========================#
+        # =========== Déclaration des variables ========================#
         self.service = StringVar()
         self.numeroDImmatriculation = StringVar()
         self.nom = StringVar()
@@ -29,9 +29,42 @@ class Hopital:
         self.heure = StringVar()
         self.nombreDeNuits = StringVar()
 
+        # =========== Table de la fin de la fenêtre ================= #
+        self.hopitalTable = ttk.Treeview(
+            columns=(
+                "service",
+                "numeroDImmatriculation",
+                "nom",
+                "prenom",
+                "adresse",
+                "age",
+                "sexe",
+                "descriptionDeLaMaladie",
+                "numeroDeLaChambre",
+                "specialiteDuMedecin",
+                "coordoneeDuMedecin",
+                "date",
+                "heure",
+                "nombreDeNuits",
+            ),
+        )
+
+        # =========== On formatte le titre ========================= #
+        lbltitle = Label(
+            self.root,
+            bd=20,
+            relief=RIDGE,
+            text="HOPITAL LES GÉNIES MANAGEMENT SYSTEM",
+            fg="red",
+            bg="white",
+            font=("times new roman", 50, "bold"),
+        )
+        lbltitle.pack(side=TOP, fill=X)
+
+        # =============================================== DataFrame ========================================================= #
 
 
-#================== Déclaration de notre TKinter====================+++
+# ================== Déclaration de notre TKinter====================#
 root = Tk()
 ob = Hopital(root)
 root.mainloop()
